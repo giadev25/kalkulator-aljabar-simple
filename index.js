88da54e1cebc7657c6c7b3839dfb7ec53154e890
giadev25 <argiamahya1602@gmail.com>
2021-07-30 21:24:44 +0700

    menambah fitur algebra simple

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(2 + 2);
+function algebra(math) {
+    let arrayMath = math.split(' ');
+    for (let i = 0; i < arrayMath.length; i++) {
+        if(arrayMath[i] == '+'){
+            console.log('tambah');
+        }   
+    } 
+}
+
+console.log(algebra('x + x'));
